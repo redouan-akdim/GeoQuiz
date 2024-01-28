0f67b56b89420859f79b2ad07b2411ea0b5770e4
Redouan <redouan.akdim@web.de>
2024-01-28 03:46:23 +0100

    Switch Toast to a Snackbar

--- app/src/main/java/com/akdim/geoquiz/MainActivity.kt
+++ app/src/main/java/com/akdim/geoquiz/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.akdim.geoquiz
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.snackbar.SnackbarContentLayout
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         falseButton = findViewById(R.id.false_button)
 
         // Define event handler for onClick to show toasts
-        trueButton.setOnClickListener {view: View ->
+        /*trueButton.setOnClickListener {view: View ->
             Toast.makeText(this,R.string.true_button,
             Toast.LENGTH_SHORT).show()
         }
@@ -28,6 +31,23 @@ class MainActivity : AppCompatActivity() {
         falseButton.setOnClickListener {view: View ->
             Toast.makeText(this,R.string.false_button,
                 Toast.LENGTH_SHORT).show()
+        }*/
+
+        // Define event handler for onClick to show snackbars
+        trueButton.setOnClickListener{
+            val snackbar = Snackbar.make(it,"Correct",Snackbar.LENGTH_LONG)
+
+            snackbar.show()
+
         }
+
+        falseButton.setOnClickListener{
+            val snackbar = Snackbar.make(it,"Incorrect",Snackbar.LENGTH_LONG)
+            snackbar.setTextColor(Color.BLACK)
+            snackbar.setBackgroundTint(Color.RED)
+
+            snackbar.show()
+        }
+
     }
 }
