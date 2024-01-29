6d68853468702468ffefe44636f7b5eaf968787d
Redouan <redouan.akdim@web.de>
2024-01-29 23:07:22 +0100

    feat:Created Question class

--- app/src/main/java/com/akdim/geoquiz/MainActivity.kt
+++ app/src/main/java/com/akdim/geoquiz/MainActivity.kt
@@ -22,18 +22,7 @@ class MainActivity : AppCompatActivity() {
         trueButton = findViewById(R.id.true_button)
         falseButton = findViewById(R.id.false_button)
 
-        // Define event handler for onClick to show toasts
-        /*trueButton.setOnClickListener {view: View ->
-            Toast.makeText(this,R.string.true_button,
-            Toast.LENGTH_SHORT).show()
-        }
-
-        falseButton.setOnClickListener {view: View ->
-            Toast.makeText(this,R.string.false_button,
-                Toast.LENGTH_SHORT).show()
-        }*/
-
-        // Define event handler for onClick to show snackbars
+        /* Define event handler for onClick to show snackbars */
         trueButton.setOnClickListener{
             val snackbar = Snackbar.make(it,"Correct",Snackbar.LENGTH_LONG)
 
---
+++ app/src/main/java/com/akdim/geoquiz/Question.kt
@@ -0,0 +1,5 @@
+package com.akdim.geoquiz
+
+import androidx.annotation.StringRes
+
+data class Question (@StringRes val textResID: Int, val answer: Boolean)
