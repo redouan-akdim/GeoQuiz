0310872cf645723095ee120e199fb2594bbc9df7
Redouan <redouan.akdim@web.de>
2024-01-30 01:23:50 +0100

    feat:Added binding to MainActivity

--- app/src/main/java/com/akdim/geoquiz/MainActivity.kt
+++ app/src/main/java/com/akdim/geoquiz/MainActivity.kt
@@ -6,31 +6,48 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import com.akdim.geoquiz.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.snackbar.SnackbarContentLayout
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var  binding : ActivityMainBinding
+    //private lateinit var trueButton: Button
+    //private lateinit var falseButton: Button
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         // Init Buttons
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        //trueButton = findViewById(R.id.true_button)
+        //falseButton = findViewById(R.id.false_button)
 
         /* Define event handler for onClick to show snackbars */
-        trueButton.setOnClickListener{
+        binding.trueButton.setOnClickListener{
             val snackbar = Snackbar.make(it,"Correct",Snackbar.LENGTH_LONG)
 
             snackbar.show()
 
         }
 
-        falseButton.setOnClickListener{
+        binding.falseButton.setOnClickListener{
             val snackbar = Snackbar.make(it,"Incorrect",Snackbar.LENGTH_LONG)
             snackbar.setTextColor(Color.BLACK)
             snackbar.setBackgroundTint(Color.RED)
@@ -38,5 +55,13 @@ class MainActivity : AppCompatActivity() {
             snackbar.show()
         }
 
+        binding.nextButton.setOnClickListener{
+            currentIndex = (currentIndex + 1) % questionBank.size       // Get the next index of questionBank
+            val questionTextResId = questionBank[currentIndex].textResID
+            binding.questionTextview.setText(questionTextResId)         // Display new question text
+        }
+
+        val questionTextResId = questionBank[currentIndex].textResID        // Access the text resource ID of the current Question object
+        binding.questionTextview.setText(questionTextResId)
     }
 }
